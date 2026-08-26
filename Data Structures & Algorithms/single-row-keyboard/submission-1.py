1a16b356d3eed0e6ec3556551b78f1435f797aa3
Rakei Kahukoti <rakeikahukoti@gmail.com>
2026-08-27 11:42:22 +1200

    Add: single-row-keyboard - submission-1

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        prev = 0
+        total = 0
+
+        for i in range(0,len(word)):
+            for x in range(0,len(keyboard)):
+                if keyboard[x] == word[i]:
+                    if prev > x:
+                        total = total + (prev - x)
+                        prev = x
+                    else:
+                        total = total + (x - prev)
+                        prev = x
+                    
+                    break
+        
+        return total
