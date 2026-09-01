d3eb0ae6a110d16f420c36951e70dfae94298bc0
Rakei Kahukoti <rakeikahukoti@gmail.com>
2026-09-02 10:39:38 +1200

    Add: next-greater-element-i - submission-2

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        temp = []
+        count = 1
+
+        for num1 in nums1:
+            for num2 in nums2:
+                if num1 == num2:
+                    for i in range(nums2.index(num2), len(nums2)):
+                        if nums2[i] > num1:
+                            temp.append(nums2[i])
+                            count = count + 1
+                            break
+                        elif i == (len(nums2) - 1):
+                            temp.append(-1)
+                    if count == len(temp):
+                        break
+        return temp
